6da0ab9c2d30ff26b23bfacf4bd99681eca77f73
arkadievvladimir <arkadiev.vladimir@gmail.com>
2020-12-19 05:26:01 +0300

    feat: use react hooks in chart component

--- covid-dashboard/src/components/MainComponent/RightSideComponent/RightSideComponent.js
+++ covid-dashboard/src/components/MainComponent/RightSideComponent/RightSideComponent.js
@@ -4,12 +4,21 @@ import TableComponent from './TableComponent/index';
 import ChartComponent from './ChartComponent/index';
 
 const RightSideComponent = () => {
-    return (
-        <div className="right-side-component-wrapper">
-            <TableComponent />
-            <ChartComponent />
-        </div>
-    );
+
+  //test data
+  let data = [];
+  let visits = 10;
+  for (let i = 1; i < 366; i++) {
+    visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 100);
+    data.push({ date: new Date(2019, 0, i), value: visits });
+  }
+ 
+  return (
+    <div className='right-side-component-wrapper'>
+      <TableComponent />
+      <ChartComponent data={data}/>
+    </div>
+  );
 };
 
 export default RightSideComponent;
